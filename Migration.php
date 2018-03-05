b717badb9317a04532dc4e574a85c4ac4d09e008
Oscar Gardiazabal <trollderiu@gmail.com>
2018-03-05 19:33:19 -0300

    Update Migration.php

--- Migration.php
+++ Migration.php
@@ -1,17 +1,12 @@
 <?php
 
+namespace Simple;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration as Illuminate_Migration;
 
 class Migration extends Illuminate_Migration {
-    /* RESET:
-     * php artisan migrate:refresh --database=migrations
-     * php artisan migrate:reset --force --database=migrations
-     */
-    
-    protected $table = "migrations";
-    protected $connection = 'migrations';
 
     protected function col($tablename, $type, $column, $extra = null) {
         if (!Schema::hasColumn($tablename, $column)) {
