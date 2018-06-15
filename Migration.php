645a9180ece77b87855e0ab54903e4d9daa7e3f2
Oscar Gardiazabal <trollderiu@gmail.com>
2018-06-15 12:23:03 -0400

    Update Migration.php

--- Migration.php
+++ Migration.php
@@ -7,11 +7,22 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration as Illuminate_Migration;
 
 class Migration extends Illuminate_Migration {
+    /* RESET:
+     * php artisan migrate:refresh
+     * php artisan migrate:reset --force
+     */
+    
+    protected $table = "migrations";
+    protected $connection = 'mysql';
 
     protected function col($tablename, $type, $column, $extra = null) {
-        if (!Schema::hasColumn($tablename, $column)) {
+        if(!is_array($column)){
+            $column = [$column];
+        }
+
+        if (!Schema::hasColumn($tablename, $column[0])) {
             Schema::table($tablename, function (Blueprint $table) use ($type, $column, $extra) {
-                $col = $table->$type($column);
+                $col = call_user_func_array([$table, $type], $column);
                 if($extra){
                     $col->$extra();
                 }
